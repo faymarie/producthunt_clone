291798b695de78fd9e6fa5c916a38520559d2174
Marie Klaus <marie.klaus@code.berlin>
2018-11-20 13:01:09 +0100

    Implement Django signup.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# for signup
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords must match.'})
+    else:
+        return render(request, 'accounts/signup.html')
+
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    # to do: route to homepage
+    return render(request, 'accounts/logout.html')
