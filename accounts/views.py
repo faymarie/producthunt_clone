f1a140528185b6e3eab0147a3e26c0396febb665
Marie Klaus <marie.klaus@code.berlin>
2018-11-20 13:23:20 +0100

    Implement signin and signout.

--- accounts/views.py
+++ accounts/views.py
@@ -19,11 +19,20 @@ def signup(request):
     else:
         return render(request, 'accounts/signup.html')
 
-
-
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
-    # to do: route to homepage
-    return render(request, 'accounts/logout.html')
+    """must be a post request as browsers re-render to be faster. A get request would cause a logout
+    without hitting the button"""
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
