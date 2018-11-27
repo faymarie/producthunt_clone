45a1cc0f56ab8d6a501f3b7dbf8907d0f505b6cd
Marie Klaus <marie.klaus@code.berlin>
2018-11-27 15:51:49 +0100

    Add product details page.

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
